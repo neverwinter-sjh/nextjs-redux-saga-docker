3f3428802c023c798e91701d97990c55f29c2a6c
neverwintersjh <neverwinter.sjh@gmail.com>
2020-11-06 09:28:32 +0900

    Hooks 테스트

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import CounterContainer from 'src/components/counter/CounterContainer';
 import { INCREASE_ASYNC } from 'src/redux/stores/counter';
+import TestHooks from 'src/components/TestHooks';
 
 const Index = (props) => {
   return (
     <div>
       <CounterContainer />
+      <hr />
+      <TestHooks />
       <hr />
       <p>from server: {props.isServer ? 'true' : 'false'}</p>
       <p>{props.value}</p>
